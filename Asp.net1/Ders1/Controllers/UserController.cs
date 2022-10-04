3d425947efbb29806fc0f06737525ce0393d8bbd
mammadzadaulvi <ulvifm@code.edu.az>
2022-10-04 18:20:44 +0400

    Asp.net 1

---
+++ Asp.net1/Ders1/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Ulvi";
+            ViewBag.Surname = "Mammadzada";
+            ViewBag.Age = 21;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "050-555-55-55";
+            return View();
+        }
+    }
+}
